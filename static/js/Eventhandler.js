dddf9636581360ce78959d52804e7fd6ee3d450b
Dominik <djuelg@gmx.de>
2016-06-28 20:51:40 +0200

    Added Eventhandler.js for Button interaction

---
+++ static/js/Eventhandler.js
@@ -0,0 +1,24 @@
+window.onload = function() {
+    var canvas = document.getElementById('canvas-cloud'),
+        ctx = canvas.getContext('2d');
+
+    function doSampleCanvas() {
+        /* draw something */
+        ctx.fillStyle = '#FF5722';
+        ctx.font = '20px sans-serif';
+        ctx.fillText('Sample Tagcloud', 10, canvas.height / 2 - 15);
+        ctx.font = '16px sans-serif';
+        ctx.fillText('Click link below to save this as image', 15, canvas.height / 2 + 35);
+    }
+
+    function downloadCanvas(link, canvasId, filename) {
+        link.href = document.getElementById(canvasId).toDataURL();
+        link.download = filename;
+    }
+
+    document.getElementById('download-link').addEventListener('click', function() {
+        downloadCanvas(this, 'canvas-cloud', 'Tagcloud.png');
+    }, false);
+
+    doSampleCanvas();
+}
